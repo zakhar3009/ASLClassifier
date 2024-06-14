9a8c8fd8a9c2a92e6c26c7f662d636e479f54c2c
zakhar3009 <zakhlit@gmail.com>
2024-06-15 00:24:00 +0300

    Initial Commit

---
+++ ASL/ASLApp.swift
@@ -0,0 +1,17 @@
+//
+//  ASLApp.swift
+//  ASL
+//
+//  Created by Zakhar Litvinchuk on 15.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct ASLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
